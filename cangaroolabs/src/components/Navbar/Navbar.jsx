ea816877064339118e6a5894988dcd575c018c96
Dulitha Wickramasinghe <110032696+Dulitha99@users.noreply.github.com>
2023-12-28 09:49:22 -0800

    10

--- cangaroolabs/src/components/Navbar/Navbar.jsx
+++ cangaroolabs/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
-    <div></div>
+    <div>
+        <a href="#home">HOME</a>
+        <a href="#blog">BLOG</a>
+        <a href="#projects">PROJECTS</a>
+        <a href="#about">ABOUT</a>
+        <a href="#contact">CONTACT US</a>
+        <button classname="login">Login</button>
+    </div>
   )
 }
 
