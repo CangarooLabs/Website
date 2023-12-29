3fb1ce86119186c62a43e338ee81c92a04e83981
Usitha5555 <usithaarachchiofficial@gmail.com>
2023-12-29 23:03:21 +0530

    commit u

--- cangaroolabs/src/components/Hompage/Hompage.jsx
+++ cangaroolabs/src/components/Hompage/Hompage.jsx
@@ -6,8 +6,11 @@ const Hompage = () => {
   return (
     <div classname="homepage">
     <Header/>
-    <div className="can"><img src='./images/3d-rendering-robotic-dog-removebg 1.png' alt='dfw'/></div>
     <div className="can2"><img src='./images/Group2.png'alt="dfw"/></div>
+    
+    
+    <div className="can"><img src='./images/3d-rendering-robotic-dog-removebg 1.png' alt='dfw'/></div>
+    
     </div>
   )
 }
