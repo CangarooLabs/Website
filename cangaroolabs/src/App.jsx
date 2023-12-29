a0b710fe90719a629c32513b3e43ac603197e174
Dulitha Wickramasinghe <110032696+Dulitha99@users.noreply.github.com>
2023-12-29 01:14:14 -0800

    12

--- cangaroolabs/src/App.jsx
+++ cangaroolabs/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.scss'
-import './components/Navbar/Navbar'
+import Navbar from'./components/Navbar/Navbar'
 
 const App = () => {
     return(
     <div className="section1">
+        <Navbar/>
+        <div className="section2">Dulitha</div>
+        <div className="section3">Dulitha1</div>
     </div>
     )
 }
--- cangaroolabs/src/components/Navbar/Navbar.jsx
+++ cangaroolabs/src/components/Navbar/Navbar.jsx
@@ -5,12 +5,13 @@ const Navbar = () => {
   return (
     <div>
         <div className="cangaroo">CANGAROO</div>
-        <a href="#home">HOME</a>
+        <div className="nav"><a href="#home">HOME</a>
         <a href="#blog">BLOG</a>
         <a href="#projects">PROJECTS</a>
         <a href="#about">ABOUT</a>
         <a href="#contact">CONTACT US</a>
         <button classname="login">Login</button>
+        </div>
     </div>
   )
 }
