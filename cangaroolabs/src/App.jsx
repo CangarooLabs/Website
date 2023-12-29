930226c0d5ea7b833ed161b2cb13100b3f452c34
Dulitha Wickramasinghe <110032696+Dulitha99@users.noreply.github.com>
2023-12-29 06:07:24 -0800

    16

--- cangaroolabs/src/App.jsx
+++ cangaroolabs/src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
         <div className="section2"><div classname="text1">What we offer</div></div>
         <div className="section3">
             <div classname="text1">Why Us</div>
-            Dulitha
             </div>
         <div className="section4"><div classname="text1">Project Highlights</div></div>
         <div className="section5"><div classname="text1">Our Team</div></div>
