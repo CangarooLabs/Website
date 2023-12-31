4eec0af06f8633a170f039c72141aa2d28899cdc
Dulitha Wickramasinghe <110032696+Dulitha99@users.noreply.github.com>
2023-12-31 04:24:13 -0800

    26

--- cangaroolabs/src/App.jsx
+++ cangaroolabs/src/App.jsx
@@ -7,13 +7,26 @@ const App = () => {
     return(
     <div>
         <div className='section1'><Homepage/></div>
-        <div className="section2"><div classname="text1"> <img src='./images/WhatWeOffer.png' alt='dfw'/></div></div>
+        <div className="section2"><div classname="text1"> <img src='./images/WhatWeOffer.png' alt='dfw'/>
+        <div className="container1">
+        <div class="row">
+    <div class="box">Box 1</div>
+    <div class="box">Box 2</div>
+    <div class="box">Box 3</div>
+</div></div>
+<div className="container2">
+<div class="row">
+    <div class="box">Box 4</div>
+    <div class="box">Box 5</div>
+    <div class="box">Box 6</div>
+</div></div></div></div>
         <div className="section3">
             <div classname="text1"><div classname="text1"> <img src='./images/Why Us.png' alt='dfw'/></div></div>
             </div>
         <div className="section4"><div classname="text1"> <img src='./images/Project Highlights.png' alt='dfw'/></div></div>
-        <div className="section5"><div classname="text1"> <img src='./images/Our Team.png' alt='dfw'/></div></div>
-        <Footer/>
+        <div className="section5"><div classname="text1"> <img src='./images/Our Team.png' alt='dfw'/>
+        <Footer/></div></div>
+       
     </div>
     
     )
