84df45a4555d9cdae138d5aadfd42db88ad6d01c
Dulitha Wickramasinghe <110032696+Dulitha99@users.noreply.github.com>
2023-12-31 08:25:15 -0800

    30

--- cangaroolabs/src/components/Hompage/Hompage.jsx
+++ cangaroolabs/src/components/Hompage/Hompage.jsx
@@ -4,15 +4,12 @@ import Header from "../Header/Header"
 import ParticleBg from '../ParticleBg'
 const Hompage = () => {
   return (
+    <div><Header/>
     <div classname="homepage">
-    <Header/>
     <ParticleBg/>
     <div className="can2"><img src='./images/Group2.png'alt="dfw"/></div>
-    
-    
     <div className="can"><img src='./images/3d-rendering-robotic-dog-removebg 1.png' alt='dfw'/></div>
-    
-    </div>
+    </div></div>
   )
 }
 
