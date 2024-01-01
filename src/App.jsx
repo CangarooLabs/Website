c06870fe7df9e87a13bc16bb35ce3d6a2f0b96e4
Usitha5555 <usithaarachchiofficial@gmail.com>
2024-01-01 23:42:33 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './components/Header/Header';
+import Header from './components/Header/Header';
+import Mainsection from './components/mainsection/Mainsection';
+import ParticleBg from './components/ParticleBg';
+import Stat from './components/Stat/Stat';
+import Services from './components/Services/Services';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Mainsection/> 
+      <Stat/>
+      
+      <Services/>
+      
+      
+      
+      
+      
+</div>
+  );
+}
+
+export default App;
